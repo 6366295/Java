e242152c0d5dd7cf15237af5588c38df12f80fa5
Mike Trieu <mt_tr1pl@hotmail.com>
2011-11-08 20:48:28 +0100

    af

--- opgave3/Opgave3.java
+++ opgave3/Opgave3.java
@@ -5,9 +5,15 @@
  *
  * Programma functie: 
  *  Functie 1:
- *  - 
+ *  - Het programma vraagt om een invoer(integer), waarmee bepaald wordt hoeveel Lucas-getallen het moet printen.
+ *  - Na het printen van de Lucas-getallen, vraagt het of je dit programma opnieuw wilt uitvoeren.
+ *  - Wanneer een invoer niet een integer is, wordt dit aangegeven
+ *  - De uitvoer is bij een bepaalde invoer te groot voor een integer. Dit wordt aangegeven.
  *  Functie 2:
- *  -
+ *  - Het programma vraagt een invoer(integer) voor de grondtal en een invoer(integer) voor de exponent.
+ *  - Hiermee wordt machtsverheffen uitgevoerd. Het berekende uitkomst zijn doubles.
+ *  - Na het printen van de uitkomst, vraagt het programma of een nieuwe berekening wilt uitvoeren.
+ *  - Wanneer een invoer niet een integer is, wordt dit aangegeven
  *
  * - Gebruik: java Opgave3
  */
@@ -80,9 +86,7 @@ public class Opgave3 {
 
 
 	/*
-	 *
-	 *
-	 *
+	 * De methode om de Lucas-reeks uit te rekenen
 	 */
 	static void lucasReeks(int aantal) {
 		int lucasEen, lucasTwee, lucasNieuw;
@@ -95,7 +99,9 @@ public class Opgave3 {
 		} else if(aantal == 1) {
 			System.out.print("De eerste " + aantal + " Lucas-getallen: " + lucasEen);
 		} else if(aantal < 1) {
-			System.out.print("De eerste 0 Lucas-getallen: ");
+			System.out.print("De eerste 0 Lucas-getallen:");
+		} else if(aantal > 45) {
+			System.out.print("De Lucas-getallen kloppen niet meer vanaf een invoer van 46!");
 		} else {
 			System.out.print("De eerste " + aantal + " Lucas-getallen: " + lucasEen + " " + lucasTwee + " ");
 			do {
@@ -111,17 +117,16 @@ public class Opgave3 {
 	}
 
 
-
 	/*
-	 *
-	 *
-	 *
+	 * De methode om te machtsverheffen.
 	 */
 	static double macht(int grondtal, int exp) {
+		int exponent;
 		double uitkomstTemp, uitkomst;
 
 		uitkomst = 1.0;
 		uitkomstTemp = 1.0;
+		exponent = exp;
 
 		if(grondtal != 0 && exp > 0) {
 			while(exp > 0) {
@@ -129,21 +134,29 @@ public class Opgave3 {
 
 				exp--;
 			}
-			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exp + " is: " + uitkomst);
-		} else if(grondtal != 0 && exp < 0) {
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: " + uitkomst);
+		} else if(grondtal > 0 && exp < 0) {
 			while(exp < 0) {
 				uitkomstTemp = grondtal * uitkomstTemp;
 				uitkomst = 1.0 / uitkomstTemp;
 
 				exp++;
 			}
-			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exp + " is: " + uitkomst);
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: " + uitkomst);
+		} else if(grondtal < 0 && exp < 0) {
+			while(exp < 0) {
+				uitkomstTemp = -grondtal * uitkomstTemp;
+				uitkomst = -(1.0 / uitkomstTemp);
+
+				exp++;
+			}
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: " + uitkomst);
 		} else if(grondtal != 0 && exp == 0) {
-			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exp + " is: 1.0");
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: 1.0");
 		} else if(grondtal == 0 && exp > 0) {
-			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exp + " is: 0.0");
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: 0.0");
 		} else {
-			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exp + " is: Ongedefineerd");
+			System.out.println("De uitkomst van " + grondtal + " tot-de-macht " + exponent + " is: Ongedefineerd");
 		}
 		return uitkomst;
 	}
